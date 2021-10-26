1fb354f9b7948057de3bd567ff72a8846b7cce6f
NoMonsters <mlx-01@yandex.ru>
2021-10-26 20:34:17 +0300

    Простейший пример с CAN
    МК сам себе отправляет CAN сообщение, фильтр натсроен на пропускание всех сообщений

--- can_test/Core/Src/main.c
+++ can_test/Core/Src/main.c
@@ -46,6 +46,12 @@ UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 
+CAN_TxHeaderTypeDef TxHeader;
+CAN_RxHeaderTypeDef RxHeader;
+uint8_t TxData[8] = {0,};
+uint8_t RxData[8] = {0,};
+uint32_t TxMailbox = 0;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -59,7 +65,30 @@ static void MX_USART1_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+	char trans_str[20] = "\0";
+    if(HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) == HAL_OK)
+    {
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
 
+        if(RxHeader.StdId == 0x0178)
+        {
+                snprintf(trans_str, 128, "ID %04lX %d\n", RxHeader.StdId, RxData[0]);
+                HAL_UART_Transmit(&huart1, (uint8_t*)trans_str, strlen(trans_str), 100);
+        }
+
+    }
+
+}
+void HAL_CAN_ErrorCallback(CAN_HandleTypeDef *hcan)
+{
+	char trans_str[20] = "\0";
+    uint32_t er = HAL_CAN_GetError(hcan);
+
+    sprintf(trans_str, "ER CAN %lu %08lX", er, er);
+    HAL_UART_Transmit(&huart1, (uint8_t*)trans_str, strlen(trans_str), 100);
+}
 /* USER CODE END 0 */
 
 /**
@@ -89,10 +118,22 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_CAN1_Init();
-  MX_USART1_UART_Init();
-  /* USER CODE BEGIN 2 */
+	MX_GPIO_Init();
+	MX_CAN1_Init();
+	MX_USART1_UART_Init();
+	/* USER CODE BEGIN 2 */
+	TxHeader.StdId = 0x0178;
+	TxHeader.ExtId = 0;
+	TxHeader.RTR = CAN_RTR_DATA; //CAN_RTR_REMOTE
+	TxHeader.IDE = CAN_ID_STD;   // CAN_ID_EXT
+	TxHeader.DLC = 8;
+	TxHeader.TransmitGlobalTime = 0;
+
+	snprintf(TxData, "Hello\n", 6);
+
+	HAL_CAN_Start(&hcan1);
+
+	HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING | CAN_IT_ERROR | CAN_IT_BUSOFF | CAN_IT_LAST_ERROR_CODE);
 
   /* USER CODE END 2 */
 
@@ -100,6 +141,27 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+		uint32_t can_state = 0, free_level = 0;
+		can_state = HAL_CAN_GetState(&hcan1);
+		free_level = HAL_CAN_GetTxMailboxesFreeLevel(&hcan1);
+
+		char trans_str[20] = "\0";
+
+		sprintf(trans_str, "%d ,%d\n", can_state, free_level);
+		HAL_UART_Transmit(&huart1, (uint8_t*)trans_str, strlen(trans_str), 100);
+		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
+
+		HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox);
+
+		/*while(HAL_CAN_GetTxMailboxesFreeLevel(&hcan1) == 0);
+
+		if(HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+		{
+			  HAL_UART_Transmit(&huart1, (uint8_t*)"ER SEND\n", 8, 100);
+		}*/
+
+		HAL_Delay(500);
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -167,14 +229,14 @@ static void MX_CAN1_Init(void)
   /* USER CODE END CAN1_Init 1 */
   hcan1.Instance = CAN1;
   hcan1.Init.Prescaler = 4;
-  hcan1.Init.Mode = CAN_MODE_NORMAL;
+  hcan1.Init.Mode = CAN_MODE_SILENT_LOOPBACK;
   hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
   hcan1.Init.TimeSeg1 = CAN_BS1_13TQ;
   hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
   hcan1.Init.TimeTriggeredMode = DISABLE;
   hcan1.Init.AutoBusOff = ENABLE;
   hcan1.Init.AutoWakeUp = DISABLE;
-  hcan1.Init.AutoRetransmission = ENABLE;
+  hcan1.Init.AutoRetransmission = DISABLE;
   hcan1.Init.ReceiveFifoLocked = DISABLE;
   hcan1.Init.TransmitFifoPriority = ENABLE;
   if (HAL_CAN_Init(&hcan1) != HAL_OK)
@@ -182,7 +244,21 @@ static void MX_CAN1_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN CAN1_Init 2 */
-
+	CAN_FilterTypeDef  sFilterConfig;
+	sFilterConfig.FilterBank = 0;
+	sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
+	sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
+	sFilterConfig.FilterIdHigh = 0x0000;
+	sFilterConfig.FilterIdLow = 0x0000;
+	sFilterConfig.FilterMaskIdHigh = 0x0000;
+	sFilterConfig.FilterMaskIdLow = 0x0000;
+	sFilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0;
+	sFilterConfig.FilterActivation = ENABLE;
+	sFilterConfig.SlaveStartFilterBank = 14;
+	if(HAL_CAN_ConfigFilter(&hcan1, &sFilterConfig) != HAL_OK)
+	{
+	Error_Handler();
+	}
   /* USER CODE END CAN1_Init 2 */
 
 }
@@ -227,10 +303,21 @@ static void MX_USART1_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PA1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
